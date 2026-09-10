5f7da3dc6e7d3df90868bc9fc93bdca5f3e9cb63
guillermo.hoysan <guillermo.hoysan@educa.jcyl.es>
2026-09-10 09:56:57 +0200

    c

---
+++ src/Aplicacion.java
@@ -0,0 +1,39 @@
+abstract class Aplicacion implements  Imprimible {
+    protected String nombre;
+    protected String version;
+    protected double pesoMB;
+
+    public  Aplicacion(String nombre, String version, double pesoMB) {
+        this.nombre = nombre;
+        this.version = version;
+        this.pesoMB = pesoMB;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public double getPesoMB() {
+        return pesoMB;
+    }
+
+    public void setPesoMB(double pesoMB) {
+        this.pesoMB = pesoMB;
+    }
+    @Override
+    public String devolverinfoString(){
+        return nombre + version + pesoMB;
+    }
+}
---
+++ src/Juego.java
@@ -0,0 +1,22 @@
+public class Juego extends Aplicacion {
+    private boolean multijugador;
+
+
+
+    public Juego(String nombre, String version, double pesoMB, boolean multijugador) {
+        super(nombre, version, pesoMB);
+        this.multijugador = multijugador;
+
+    }
+    public boolean isMultijugador() {
+        return multijugador;
+    }
+
+    public void setMultijugador(boolean multijugador) {
+        this.multijugador = multijugador;
+    }
+    @Override
+    public String devolverinfoString(){
+        return nombre + " " + version + " " + pesoMB + " "+ multijugador;
+    }
+}
